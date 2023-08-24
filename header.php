680a931433dbc0216f1f003cda37616366cce4ea
Dev Patel <141422670+Dev-infilon@users.noreply.github.com>
2023-08-24 15:01:27 +0530

    Adding Header and Footer

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/index.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="banner">
+        <img class="icon" src="assets/icons/Location.svg" alt="location icon">
+        <p> 24 Olive Street, Prairie, NY 53590 </p>
+
+        <img class="icon1" src="assets/icons/Email.svg" alt="Email icon">
+        <p class="banner1"> whitecollar@gmail.com </p>
+
+        <div class="banner2"> <img class="icon1" src="assets/icons/Time.svg" alt="Clock icon">
+            <p> Mon - Fri: 8:00 am - 5:00 pm </p>
+        </div>
+
+        <div class="sm">
+            <img src="assets/icons/Insta.svg" alt="insta icon">
+            <img src="assets/icons/Fb.svg" alt="Fb icon">
+            <img src="assets/icons/Twiter.svg" alt="Twiter icon">
+            <img src="assets/icons/Pintrest.svg" alt="Pintrest icon">
+        </div>
+    </div>
